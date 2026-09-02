b8f1a0c671d1e16c6d7d7c60ec071f238274fa67
nicolasluarte <64244503+nicolasluarte@users.noreply.github.com>
2026-09-02 01:30:53 -0400

    setup

--- cerebellum_project/src/r/stan_model_fit.R
+++ cerebellum_project/src/r/stan_model_fit.R
@@ -1,6 +1,19 @@
+# setup -------------------------------------------------------------------
+local_lib <- Sys.getenv("R_LIBS_USER")
+
+if (!dir.exists(local_lib)) {
+  dir.create(local_lib, recursive = TRUE)
+}
+.libPaths(c(local_lib, .libPaths()))
+
 # libs --------------------------------------------------------------------
 cat("LIBS")
-if (!require("pacman", character.only = TRUE)) install.packages("pacman")
+if (!require("pacman", character.only = TRUE)) {
+  install.packages("pacman",
+                   lib = local_lib,
+                   repos = "http://cran.us.r-project.org")
+  library("pacman", character.only = TRUE)
+}
 pacman::p_load(
   tidyverse,
   cmdstanr,
