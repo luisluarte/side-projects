f5c2b6836d391236c4e143a4930e44eb1a6e2eba
nicolasluarte <64244503+nicolasluarte@users.noreply.github.com>
2026-09-02 03:04:05 -0400

    update

--- cerebellum_project/src/r/model_stats.R
+++ cerebellum_project/src/r/model_stats.R
@@ -10,7 +10,8 @@ if (!dir.exists(local_lib)) {
 cat("LIBS\n")
 if (!require("pacman", character.only = TRUE)) {
   install.packages("pacman",
-                   lib = local_lib)
+    lib = local_lib
+  )
   library("pacman", character.only = TRUE)
 }
 pacman::p_load(
@@ -47,35 +48,34 @@ get_median <- function(fit, prefix, n_subj) {
 N_subj <- length(unique(dat_raw$participant_id))
 
 # V-OPT Parameters
-v_abase   <- get_median(draws_vopt, "a_base_raw", N_subj)
-v_vctx    <- get_median(draws_vopt, "v_ctx", N_subj)
-v_wbias   <- get_median(draws_vopt, "w_bias_raw", N_subj)
-v_aw      <- get_median(draws_vopt, "aw", N_subj)
-v_al      <- get_median(draws_vopt, "al", N_subj)
-v_wctx    <- get_median(draws_vopt, "w_ctx", N_subj)
+v_abase <- get_median(draws_vopt, "a_base_raw", N_subj)
+v_vctx <- get_median(draws_vopt, "v_ctx", N_subj)
+v_wbias <- get_median(draws_vopt, "w_bias_raw", N_subj)
+v_aw <- get_median(draws_vopt, "aw", N_subj)
+v_al <- get_median(draws_vopt, "al", N_subj)
+v_wctx <- get_median(draws_vopt, "w_ctx", N_subj)
 v_betamis <- get_median(draws_vopt, "beta_mismatch", N_subj)
 # M012 Parameters
-m_abase   <- get_median(draws_m012, "a_base_raw", N_subj)
-m_vctx    <- get_median(draws_m012, "v_ctx", N_subj)
-m_wbias   <- get_median(draws_m012, "w_bias_raw", N_subj)
-m_aw      <- get_median(draws_m012, "aw", N_subj)
-m_al      <- get_median(draws_m012, "al", N_subj)
-m_tau     <- get_median(draws_m012, "tau", N_subj) # or tau_decay depending on what you named it
+m_abase <- get_median(draws_m012, "a_base_raw", N_subj)
+m_vctx <- get_median(draws_m012, "v_ctx", N_subj)
+m_wbias <- get_median(draws_m012, "w_bias_raw", N_subj)
+m_aw <- get_median(draws_m012, "aw", N_subj)
+m_al <- get_median(draws_m012, "al", N_subj)
+m_tau <- get_median(draws_m012, "tau", N_subj) # or tau_decay depending on what you named it
 m_alphaPC <- get_median(draws_m012, "alpha_pc", N_subj)
-m_wctx    <- get_median(draws_m012, "w_ctx", N_subj)
-m_wcb     <- get_median(draws_m012, "w_cb", N_subj)
+m_wctx <- get_median(draws_m012, "w_ctx", N_subj)
+m_wcb <- get_median(draws_m012, "w_cb", N_subj)
 m_betamis <- get_median(draws_m012, "beta_mismatch", N_subj)
-m_g       <- get_median(draws_m012, "golgi_scale", N_subj)
+m_g <- get_median(draws_m012, "golgi_scale", N_subj)
 
 # M012 Constants
-m_frac     <- 0.1 + 0.8 * (0:3 / 3.0)
+m_frac <- 0.1 + 0.8 * (0:3 / 3.0)
 m_inv_frac <- 1.0 - m_frac
-m_kappa    <- 0.1 + 0.89 * (0:3 / 3.0)
+m_kappa <- 0.1 + 0.89 * (0:3 / 3.0)
 
-
-
-
-
+## preds -------------------------------------------------------------------
+pred_matrix_vopt <- as_draws_matrix(vopt$draws("pred_sw"))
+pred_matrix_m012 <- as_draws_matrix(m012$draws("pred_sw"))
 
 
 # stats -------------------------------------------------------------------
@@ -104,12 +104,3 @@ true_labels <- factor(
   ),
   levels = c("stay", "switch")
 )
-
-
-
-
-
-
-
-
-
